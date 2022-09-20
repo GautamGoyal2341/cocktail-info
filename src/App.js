f8aca4ace29b0684c4ab6c9b757e87367f83f9eb
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-09-20 16:03:34 +0530

    mini change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,17 @@ function App() {
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
+          target="_
+          \
+          
+          
+          
+          
+          
+          
+          
+          
+          blank"
           rel="noopener noreferrer"
         >
           Learn React
